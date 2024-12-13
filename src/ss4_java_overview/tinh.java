d9908897687a0b850638175209d8c740004e0069
vannhandz <nhan03102003@gmail.com>
2024-12-13 17:44:27 +0700

    message tương ứng

--- src/ss4_java_overview/tinh.java
+++ src/ss4_java_overview/tinh.java
@@ -1,4 +1,20 @@
 package ss4_java_overview;
 
+import java.util.Scanner;
+
 public class tinh {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("nhap so a: ");
+        double a = sc.nextDouble();
+        System.out.print("nhap so b: ");
+        double b = sc.nextDouble();
+
+        System.out.println("tổng a + b: "+ (a+b));
+        System.out.println("tổng a + b: "+ (a-b));
+        System.out.println("tổng a + b: "+ (a*b));
+        System.out.println("tổng a + b: "+ (a/b));
+
+    }
 }
