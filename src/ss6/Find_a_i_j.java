07113e1f695450952d486aaf2de69be192fa7ec5
vannhandz <nhan03102003@gmail.com>
2024-12-13 23:15:20 +0700

    ok

---
+++ src/ss6/Find_a_i_j.java
@@ -0,0 +1,36 @@
+package ss6;
+
+public class Find_a_i_j {
+    public static void main(String[] args) {
+
+// a      int i=1 ,j=1;
+//        int a=i++ +j++;
+//        System.out.println(a);
+
+
+// b      int i=1 ,j=1;
+//        int a=i++ + ++j;
+//        System.out.println(a);
+
+//  c      int i=1 ,j=1;
+//        int a=++i + j++;
+//        System.out.println(a);
+
+
+//  d     int i=1 ,j=1;
+//        int a=++i + ++j;
+//        System.out.println(a);
+
+
+// e       int i=1 ,j=1;
+//        int a=i++ +j++ + i++ +j++;
+//        System.out.println(a);
+
+ //f      int i=1 ,j=1;
+//        int a=++i + ++j + i++ +j++;
+//        System.out.println(a);
+
+
+
+    }
+}
